f171ef21a29e1875abed5abd6388ac1e3cd6cf64
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-09-07 17:15:13 +0200

    feat: add component title

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+  data() {
+    return {
+      title: "",
+    };
+  },
+  components: { AppTitle },
+};
+</script>
+
+<template>
+  <AppTitle></AppTitle>
+</template>
+
+<style></style>
